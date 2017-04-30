ac0d48f2a0955d7004b79306dfc487be1a05e634
Corina Ciocanea <corina.ciocanea@gmail.com>
2017-04-30 17:12:12 +0300

    tema

--- Homework Ch4+Ch5/HomeworkConsole/Program.cs
+++ Homework Ch4+Ch5/HomeworkConsole/Program.cs
@@ -6,17 +6,7 @@ namespace HomeworkConsole
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Give file path:");
-            string fileName = System.Console.ReadLine();
-
-            using (StreamReader reader = new StreamReader(File.Open(fileName, FileMode.Open)))
-            {
-                while (reader.EndOfStream == false)
-                {
-                    string line = reader.ReadLine();
-                    var values = line.Split(',');
-                }
-            }
+           
         }
     }
 }
